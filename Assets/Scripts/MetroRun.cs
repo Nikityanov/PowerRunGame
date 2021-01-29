e64ec4faefc3c78553c169007af499b3680b5f15
Nikityanov <dnikityanov@gmail.com>
2021-01-30 00:00:18 +0300

    Сделал примитивнуб работу механики бега в метро.

--- Assets/Scripts/MetroRun.cs
+++ Assets/Scripts/MetroRun.cs
@@ -1,21 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MetroRun : MonoBehaviour
 {
     public GameObject player;
     public float speed;
+    public Button runButton;
+    public Text metersText;
+    public Text speedText;
 
+
+   void RunOnClick()
+    {
+        speed = speed + 0.1f;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        runButton.onClick.AddListener(RunOnClick);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        player.transform.position = new Vector3(0, 0, speed * Time.deltaTime);
+    { if (speed < 20 ) {
+            metersText.text = ("Meters" + Mathf.Round(transform.position.z));
+            speedText.text = ("Speed" + Mathf.Round(speed));
+            speed = speed - 0.01f;
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+
+        if (speed == 0)
+        {
+            speed = 0;
+            Debug.Log("Тебя поймали");
+        }
     }
 }
