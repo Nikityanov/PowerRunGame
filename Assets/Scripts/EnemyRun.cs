46c0723b0ab6611e2ac44f34a81cb4b879ae0c3d
Nikityanov <dnikityanov@gmail.com>
2021-01-30 20:30:00 +0300

    Добавил врага в метро, он догоняет героя, есть рабочая коллизия

---
+++ Assets/Scripts/EnemyRun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRun : MonoBehaviour
+{
+
+    public float speed;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Hero"))
+        {
+            Debug.Log("Тебя догнали, петушара");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
