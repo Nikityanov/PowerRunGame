2ecfe5f9e4d24220d068a5269a2fc4f5ef85cdc6
Nikityanov <dnikityanov@gmail.com>
2021-01-30 20:46:12 +0300

    ПОправил индикатор скорости, прикрутил правильную анимацию персонажа, добавил изменение скорости анимации в зависимости от скорости движения

--- Assets/Scripts/MetroRun.cs
+++ Assets/Scripts/MetroRun.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class MetroRun : MonoBehaviour
 {
@@ -11,10 +12,13 @@ public class MetroRun : MonoBehaviour
     public Text metersText;
     public Text speedText;
 
+    public Animator anim;
+
 
    void RunOnClick()
     {
-        speed = speed + 0.1f;
+        speed = speed + 0.05f;
+        anim.speed += 0.05f;
     }
     // Start is called before the first frame update
     void Start()
@@ -25,9 +29,11 @@ public class MetroRun : MonoBehaviour
     // Update is called once per frame
     void Update()
     { if (speed < 20 ) {
+           float speedround = (float)Math.Round(speed, 2);
             metersText.text = ("Meters" + Mathf.Round(transform.position.z));
-            speedText.text = ("Speed" + Mathf.Round(speed));
+            speedText.text = ("Speed" + speedround);
             speed = speed - 0.01f;
+            anim.speed -= 0.01f;
             transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
         }
 
